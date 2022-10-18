e76e4ba3373596d5a08096810c69e69b25bbe5e3
moganda12 <115957608+moganda12@users.noreply.github.com>
2022-10-18 16:47:57 -0700

    you can quit now

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ see_recpticle = dipay.set_mode((width, hite))
 
 while True:
     for event in pygame.event.get() :
-        if event == pygame.QUIT :
+        if event.type == pygame.QUIT :
             pygame.quit()
             exit()
 
