37a049846775dd09ab6fba7de4ca896f242a043c
moganda12 <115957608+moganda12@users.noreply.github.com>
2022-10-18 13:40:13 -0700

    you can quit now

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ width = 800
 hite = 400
 
 see_recpticle = dipay.set_mode((width, hite))
+
+while True:
+    for event in pygame.event.get() :
+        if event == pygame.QUIT :
+            pygame.quit()
+
+    dipay.update()
