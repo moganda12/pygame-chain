329b87481badfef634d7e4119eba0880b9000e11
moganda12 <115957608+moganda12@users.noreply.github.com>
2022-10-19 13:35:22 -0700

    very logical

--- logic_manager.py
+++ logic_manager.py
@@ -4,5 +4,12 @@ class Sprite :
     showList = []
     hideList = []
 
-    def show(self, surface) :
-        n
+    def show(self, name, coord) :
+        Surface = None
+        Surface.name = name
+        Surface.coord = coord
+        self.showList.append(Surface)
+
+    def update(self) :
+        for surface in self.showList :
+            main.see_recepticle.blit(surface.name, surface.coord) 
